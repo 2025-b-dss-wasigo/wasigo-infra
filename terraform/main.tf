b231e8f0ca910a2415d515aaac585aa5cc1049d6
XolHed3RP <105742572+arielai91@users.noreply.github.com>
2025-12-26 13:44:45 -0500

    Update AKS VM size and add database namespace
    - Changed the VM size of the default node pool in the AKS configuration from Standard_B2s to Standard_B2ms for improved performance.
    - Created a new Kubernetes namespace configuration file for the database namespace.

--- terraform/main.tf
+++ terraform/main.tf
@@ -31,7 +31,7 @@ resource "azurerm_kubernetes_cluster" "this" {
   default_node_pool {
     name            = "system"
     node_count      = 1
-    vm_size         = "Standard_B2s"
+    vm_size         = "Standard_B2ms"
     os_disk_size_gb = 32
     type            = "VirtualMachineScaleSets"
   }
